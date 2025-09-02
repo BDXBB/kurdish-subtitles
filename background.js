6787a2027202899dab8a6a6ad1442919247e77cb
silver <theathprograms@gmail.com>
2025-09-02 23:04:58 +0300

    Add background script for translation on page load

---
+++ background.js
@@ -0,0 +1,26 @@
+// We Need Somthing Make Our Extension Working When We page load"Reload The Pages
+
+function MsgToContent(message) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0] && tabs[0].id) {
+        chrome.tabs.sendMessage(tabs[0].id, message);
+        }
+    });
+    }
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    // console.log("Rs") Debagging I know But It Working < ` _ ` > 
+  if (changeInfo.status === "complete") {
+    // console.log("Rss")
+    chrome.storage.sync.get(['translationEnabled', 'Tolanguagevalue'], (result) => {
+      const isEnabled = result.translationEnabled === true;
+      const lang = result.Tolanguagevalue || 'ckb';
+
+      if (isEnabled) {
+        //   console.log("Rssssas")
+         MsgToContent({ type: 'LANGUAGE_IS', language: lang });
+         MsgToContent({ type: 'TOGGLE_IS', enabled: isEnabled });
+      }
+    });
+  }
+});
